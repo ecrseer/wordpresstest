32235f85f5975a239b9c6f828bd9f5c32ed4e38f
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-20 12:59:15 -0300

    descricao

--- myga.php
+++ myga.php
@@ -6,11 +6,11 @@
  */
 /*
 Plugin Name: mymyga
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Plugin URI: http://#
+Description: testando
 Author: gab 3
 Version: 1.7.2
-Author URI: http://ma.tt/
+Author URI: http://github.com/ecrseer
 */
 function someth_ing($content){
     if(is_single()){
